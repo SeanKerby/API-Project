aa4270bebe3bab14809ea9b70be73d829ecade48
Empis <empisluramae3@gmail.com>
2025-09-08 11:01:55 +0800

    added hotel.js

--- hotel.js
+++ hotel.js
@@ -8,11 +8,19 @@ let nextHotelId = 1;
 let nextReservationId = 1;
 
 // Create hotel
-// Create hotel
 app.post("/hotels", (req, res) => {
   console.log("Incoming body:", req.body);
   const { name, location, rooms } = req.body;
   if (!name || !location || typeof rooms !== "number") {
     return res.status(400).json({ error: "name, location, rooms(required, rooms must be number)" });
   }
+  const hotel = { id: nextHotelId++, name, location, rooms };
+  hotels.push(hotel);
+  console.log("Hotel created:", hotel);
+  res.status(201).json(hotel);
+});
+
+// List hotels
+app.get("/hotels", (req, res) => {
+  res.json(hotels);
 });
